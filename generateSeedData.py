c2ccc2e8a796d97e70b24dd2d699ce715803cca9
Nirmal Bhandari <bhdnirmal99@gmail.com>
2024-02-15 14:59:21 +0000

    same data for every run

--- generateSeedData.py
+++ generateSeedData.py
@@ -1,4 +1,5 @@
 import jwt
+import json
 import pandas as pd
 from datetime import datetime, timedelta
 from pymongo import MongoClient
@@ -17,12 +18,24 @@ from bson.objectid import ObjectId
 
 # args = parser.parse_args()
 
-
 #DATA TO REMEMBER so each run of this file pushes same data to the database
-habits_data = []
-logs_data = []
-workout_types_data = []
-workout_data = []
+all_data = {
+    "habits_data": [],
+    "logs_data": [],
+    "workout_types_data": [],
+    "workout_data": []
+}
+
+try:
+    with open('all_data.json', 'r') as f:
+        file_data = json.load(f)
+        all_data.update(file_data)
+
+except (FileNotFoundError, json.JSONDecodeError):
+    print("No file found or file is empty.")
+    pass
+
+
 
 #connect to the mongo database
 client = MongoClient('mongodb://localhost:27017/')
@@ -50,25 +63,46 @@ for i, cookie in enumerate(cookies):
 
 #SETTING HABITS FOR ALL USERS
 
-def upload_user_habit_data(user_ids):
+def upload_user_habit_data(user_ids, habits):
     collection= db["coll_user_habits"]
-    for uid in user_ids:
-        data = {
-        "uid": uid,
-        "habits": habits    
-        }
-        habits_data.append(data)
-        collection.insert_one(data)
+    all_habits_docs = []
+    if len(all_data["habits_data"]) > 0:
+        print("Data already exists")
+        for habits in all_data["habits_data"]:
+            del habits["_id"]
+        insert_ids = collection.insert_many(all_data["habits_data"]).inserted_ids
+        df = pd.DataFrame(insert_ids)
+        df.to_csv("coll_user_habits.csv", index=False)
+        return True
+    else:
+        for uid in user_ids:
+            data = {
+            "uid": uid,
+            "habits": habits    
+            }
+            all_data["habits_data"].append(data)
+            all_habits_docs.append(data)
+        insert_ids = collection.insert_many(all_habits_docs).inserted_ids
+        df = pd.DataFrame(insert_ids)
+        df.to_csv("coll_user_habits.csv", index=False)
+        return True
 
-
-def delete_user_habit_data():
-    for uid in user_ids:
-        collection = db["coll_user_habits"]
-        collection.delete_many({"uid": uid})
-
+    
 def upload_user_logs_data(user_ids, habits, dates):
     collection = db["coll_logs"]
-    all_logs = [] #TODO check if data already exists in seed data
+    all_logs = []
+    if len(all_data["logs_data"]) > 0:
+        print("Data already exists")
+        all_data["logs_data"] = all_data["logs_data"]
+        for logs in all_data["logs_data"]:
+            logs["uploadDateAndTime"] = datetime.strptime(logs["uploadDateAndTime"],'%Y-%m-%dT%H:%M:%S.%f')
+            del logs["_id"]
+        insert_ids = collection.insert_many(all_data["logs_data"]).inserted_ids
+        df = pd.DataFrame(insert_ids)
+        df.to_csv("coll_logs.csv", index=False)
+        return True
+    
+    #Create Dummy Data and upload to the database
     for uid in user_ids:
         for date in dates:
             log = {key: random.choice([True, False]) for key in habits}
@@ -76,27 +110,38 @@ def upload_user_logs_data(user_ids, habits, dates):
             log["moods"] = [random.choice([True, False]) for _ in range(len(habits))]
             log["uploadDateAndTime"] = date
             all_logs.append(log)
-    return collection.insert_many(all_logs).inserted_ids
+    all_data["logs_data"] = all_logs
+    insert_ids = collection.insert_many(all_logs).inserted_ids
+    df = pd.DataFrame(insert_ids)
+    df.to_csv("coll_logs.csv", index=False)
+    return True
             
-
-# allInsertIds = upload_user_logs_data(user_ids, habits, dates)
-# df = pd.DataFrame(allInsertIds)
-# df.to_csv("log_insert_ids.csv", index=False)
-
 
 def upload_user_workout_types_data(user_ids, value):
     workout_types = ["pull", "push", "legs", "cardio"]
     collection = db["coll_user_workout_types"]
     all_workout_types = []
+    if len(all_data["workout_types_data"]) > 0:
+        print("Data already exists")
+        all_data["workout_types_data"] = all_data["workout_types_data"]
+        for workout in all_data["workout_types_data"]:
+            del workout["_id"]
+        insert_ids = collection.insert_many(all_data["workout_types_data"]).inserted_ids
+        df = pd.DataFrame(insert_ids)
+        df.to_csv("coll_user_workout_types.csv", index=False)
+        return True
+    #create Dummy Data
     for uid in user_ids:
         data = {key: value[f"{key}"] for key in workout_types}
         data["uid"] = uid
         all_workout_types.append(data)
-    return collection.insert_many(all_workout_types).inserted_ids
+    all_data["workout_types_data"] = all_workout_types
+    insert_ids = collection.insert_many(all_workout_types).inserted_ids
+    df = pd.DataFrame(insert_ids)
+    df.to_csv("coll_user_workout_types.csv", index=False)
+    return True
 
-# allWorkoutTypeIds = upload_user_workout_types_data(user_ids, workout_values)
-# df = pd.DataFrame(allWorkoutTypeIds)
-# df.to_csv("workout_type_insert_ids.csv", index=False)
+
 
 def create_set(type):
     value = random.randint(1, 100)
@@ -113,10 +158,20 @@ def create_exercise_data(name, type):
     return exercise
 
 
-def upload_wokrout_data(user_ids, values):
+def upload_workout_data(user_ids, values):
     collection = db["coll_workout_data"]
     all_workout_data = []
     workout_types = ["pull", "push", "legs", "cardio"]
+    if len(all_data["workout_data"]) > 0:
+        print("Data already exists")
+        all_data["workout_data"] = all_data["workout_data"]
+        for workout in all_data["workout_data"]:
+            workout["uploadDateAndTime"] = datetime.strptime(workout["uploadDateAndTime"],'%Y-%m-%dT%H:%M:%S.%f')
+            del workout["_id"]
+        insert_ids = collection.insert_many(all_data["workout_data"]).inserted_ids
+        df = pd.DataFrame(insert_ids)
+        df.to_csv("coll_workout_data.csv", index=False)
+        return True
     for uid in user_ids:
         for type in workout_types:
             for date in dates:
@@ -137,12 +192,11 @@ def upload_wokrout_data(user_ids, values):
                 for exercise in values[type]:
                     temp["data"].append(create_exercise_data(exercise, type))
                 all_workout_data.append(temp)
-            
-    return collection.insert_many(all_workout_data).inserted_ids
-
-allWorkoutDataIDs = upload_wokrout_data(user_ids, workout_values)
-df = pd.DataFrame(allWorkoutDataIDs)
-df.to_csv("all_workout_data_ids.csv", index=False)
+    all_data["workout_data"] = all_workout_data   
+    insert_ids = collection.insert_many(all_workout_data).inserted_ids
+    df = pd.DataFrame(insert_ids)
+    df.to_csv("coll_workout_data.csv", index=False)
+    return True
 
 
 
@@ -150,8 +204,39 @@ def delete_by_id(collection, ids):
     for id in ids:
         collection.delete_one({"_id": ObjectId(id)})
 
-# insert_ids = pd.read_csv("all_workout_data_ids.csv")
-# first_column = insert_ids.iloc[:, 0]
-# first_column_list = first_column.tolist()
 
-# delete_by_id(db["coll_workout_data"], first_column_list)
+
+def delete_previous_upload_data(coll_names):
+    for coll_name in coll_names:
+        insert_ids = pd.read_csv(f"{coll_name}.csv")
+        first_column = insert_ids.iloc[:, 0].tolist()
+        delete_by_id(db[coll_names], first_column)
+    
+        
+def delete_all(collection_name):
+    collection = db[collection_name]
+    collection.delete_many({})
+
+
+upload_user_habit_data(user_ids, habits)
+upload_user_logs_data(user_ids, habits, dates)
+upload_user_workout_types_data(user_ids, workout_values)
+upload_workout_data(user_ids, workout_values)
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        elif isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
+# Open the JSON file
+with open('all_data.json', 'w') as f:
+    # Write the data to the file
+   f.write(JSONEncoder().encode(all_data))
+
+# delete_all("coll_user_habits")
+# delete_all("coll_logs")
+# delete_all("coll_user_workout_types")
+# delete_all("coll_workout_data")
